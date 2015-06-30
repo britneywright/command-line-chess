4e67082b323ef1ba90e4c023e74685dabe980be2
Britney Wright <blw06g@gmail.com>
2015-06-30 16:52:49 -0400

    write possible_moves_from_location. Currently calls hard-coded possible_moves method"

--- lib/chess/cli.rb
+++ lib/chess/cli.rb
@@ -1,6 +1,8 @@
 require 'chess'
 class Chess
   class CLI
+    attr_reader :game
+
     def initialize(stdin,stdout)
       @stdin, @stdout = stdin, stdout
       @game = Chess::Game.new
@@ -10,18 +12,32 @@ class Chess
      #player turn sequence:
       @stdout.puts current_player_turn
       @stdout.puts prompt_player_for_origin
-      possible_moves_from_location
+      @stdout.puts possible_moves_from_location
       prompt_player_for_destination
       piece_in_destination_location
       0
     end
     
     def current_player_turn
-      "#{@game.current_player}'s turn."
+      "#{game.current_player}'s turn."
     end
 
     def prompt_player_for_origin
-      "#{@game.current_player}, your move?"
+      "#{game.current_player}, your move?"
+    end
+
+    def possible_moves_from_location
+      "moves for #{game.current_player} pawn: #{possible_moves.shift.join(', ')}"
+    end
+
+    def possible_moves
+      predetermined = [
+        ["f3","f4"],
+        ["e5","e6"],
+        ["g3","g4"],
+        ["e7","f6","g5","h4"]
+      ]
+      predetermined
     end
   end
 end
