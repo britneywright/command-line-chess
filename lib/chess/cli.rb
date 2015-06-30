752755b4124219222d2457b47cf3b41b7a377afa
Britney Wright <blw06g@gmail.com>
2015-06-30 16:20:59 -0400

    pseudocode a player's turn sequence

--- lib/chess/cli.rb
+++ lib/chess/cli.rb
@@ -5,6 +5,12 @@ class Chess
     end
 
     def call
+     #player turn sequence:
+      current_player_turn
+      prompt_player_for_origin
+      possible_moves_from_location
+      prompt_player_for_destination
+      piece_in_destination_location
       0
     end
     
