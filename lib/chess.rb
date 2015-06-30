7f96c4325f79931130994889746506ba7996d474
Britney Wright <blw06g@gmail.com>
2015-06-30 18:06:24 -0400

    create board and initial view

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,11 +1,34 @@
 class Chess
   class Game
+
+    attr_reader :board
+
     def initialize
       @current_player = current_player
+      @board = board
     end
 
     def current_player
       ["white","black"].first
+    end
+
+    def board
+      board = (0...8).map do |row|
+        (0...8).map do |column|
+          " "
+        end
+      end
+      board[0] = ["♜","♞","♝","♛","♚","♝","♞","♜"] 
+      board[1] = 8.times.map {"♟"}
+      board[-2] = 8.times.map {"♙"}
+      board[-1] = ["♖","♘","♗","♕","♔","♗","♘","♖"]
+      board
+    end
+
+    def to_s
+      printed_board = board.map.each_with_index do |row,i|
+        (8 - i).to_s + "\s\s" + row.join("  ")
+      end.join("\n") + "\n   a  b  c  d  e  f  g  h"
     end
   end
 end
