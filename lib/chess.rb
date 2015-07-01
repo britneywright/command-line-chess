4faa0e423a4d10d824ac60336bd8d93e980844a8
Britney Wright <blw06g@gmail.com>
2015-07-01 10:05:45 -0400

    add methods to convert user passed location to board coordinates and move chessman

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,13 +1,13 @@
 class Chess
   class Game
 
-    attr_reader :board, :status
-    attr_accessor :players, :current_player
+    attr_reader :status
+    attr_accessor :board, :players, :current_player
 
     def initialize
+      @board = make_board
       @players = ["white","black"]
       @current_player = players.first 
-      @board = board
       @status = predefined
     end
 
@@ -23,7 +23,7 @@ class Chess
       status.shift
     end
 
-    def board
+    def make_board
       board = (0...8).map do |row|
         (0...8).map do |column|
           " "
@@ -36,8 +36,22 @@ class Chess
       board
     end
 
+    def move(origin,destination)
+      board[convert_y(destination)][convert_x(destination)] = board[convert_y(origin)][convert_x(origin)]
+      board[convert_y(origin)][convert_x(origin)] = " "
+      board
+    end
+
+    def convert_x(chess_coord)
+      chess_coord[0].to_s.ord - 'a'.ord 
+    end
+
+    def convert_y(chess_coord)
+      8 - (chess_coord[1].to_i)
+    end 
+
     def to_s
-      printed_board = board.map.each_with_index do |row,i|
+      printed_board = board.dup.map.each_with_index do |row,i|
         (8 - i).to_s + "\s\s" + row.join("  ")
       end.join("\n") + "\n   a  b  c  d  e  f  g  h"
     end
