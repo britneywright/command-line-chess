19c55aca20ca85959a97fc788a3d10b0dc9bc758
Britney Wright <blw06g@gmail.com>
2015-07-01 12:03:20 -0400

    refactor make_board method to use Chessman objects

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,4 @@
+require_relative 'chess/chessman'
 class Chess
   class Game
 
@@ -26,16 +27,34 @@ class Chess
     def make_board
       board = (0...8).map do |row|
         (0...8).map do |column|
-          " "
+          Chess::Chessman.new("empty","space")
         end
       end
-      board[0] = ["♜","♞","♝","♛","♚","♝","♞","♜"] 
-      board[1] = 8.times.map {"♟"}
-      board[-2] = 8.times.map {"♙"}
-      board[-1] = ["♖","♘","♗","♕","♔","♗","♘","♖"]
+      board[0] = [
+        Chess::Chessman.new("black","rook"),
+        Chess::Chessman.new("black","knight"),
+        Chess::Chessman.new("black","bishop"),
+        Chess::Chessman.new("black","queen"),
+        Chess::Chessman.new("black","king"),
+        Chess::Chessman.new("black","bishop"),
+        Chess::Chessman.new("black","knight"),
+        Chess::Chessman.new("black","rook"),
+      ]
+      board[1] = 8.times.map {Chess::Chessman.new("black","pawn")}
+      board[-2] = 8.times.map {Chess::Chessman.new("white","pawn")}
+      board[-1] = [
+        Chess::Chessman.new("white","rook"),
+        Chess::Chessman.new("white","knight"),
+        Chess::Chessman.new("white","bishop"),
+        Chess::Chessman.new("white","queen"),
+        Chess::Chessman.new("white","king"),
+        Chess::Chessman.new("white","bishop"),
+        Chess::Chessman.new("white","knight"),
+        Chess::Chessman.new("white","rook")
+      ]
       board
     end
-
+    
     def move(origin,destination)
       board[convert_y(destination)][convert_x(destination)] = board[convert_y(origin)][convert_x(origin)]
       board[convert_y(origin)][convert_x(origin)] = " "
@@ -49,6 +68,10 @@ class Chess
     def convert_y(chess_coord)
       8 - (chess_coord[1].to_i)
     end 
+
+    def piece(location)
+      board[convert_y(location)][convert_x(location)].type 
+    end
 
     def to_s
       printed_board = board.dup.map.each_with_index do |row,i|
