d16373968a30f6093ccd48cf29ff5d987a4b67cb
Britney Wright <blw06g@gmail.com>
2015-07-01 11:43:05 -0400

    move chessman knowledge into Chessman class

---
+++ lib/chess/chessman.rb
@@ -0,0 +1,33 @@
+class Chess
+  class Chessman
+    attr_reader :avatar
+    def initialize(color,type)
+      @color = color
+      @type = type
+      @avatar = chessmen["#{color} #{type}"]
+    end
+
+    def chessmen
+       avatars = {
+         "black pawn" => "♟",
+         "black rook" => "♜",
+         "black knight" => "♞",
+         "black bishop" => "♝",
+         "black queen" => "♛",
+         "black king" => "♚",
+         "white pawn" => "♙",
+         "white rook" => "♖",
+         "white knight" => "♘",
+         "white bishop" => "♗",
+         "white queen" => "♕",
+         "white king" => "♔",
+         "empty space" => " "
+       }
+       avatars
+    end
+
+    def to_s
+      avatar
+    end
+  end
+end
