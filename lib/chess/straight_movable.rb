6c3d9fb7d0628ea7e7368cfff62e036fe50355a6
Britney Wright <blw06g@gmail.com>
2015-07-02 02:22:02 -0400

    refactor to work for horizontal, vertical and diagonal paths

--- lib/chess/straight_movable.rb
+++ lib/chess/straight_movable.rb
@@ -1,23 +1,16 @@
 module StraightMovable
-  def horizontal_moves(y,x)
+  def straight_moves(y,x,board,directions,limit = nil)
     possibilities = []
-    counter = 0 
-    until counter >= 8
-      possibilities << [y,counter]
-      counter += 1
+    directions.each do |go|
+      location = [y,x]
+      until location.any? {|a| a >= 7 || a < 1}
+        location = [location,go].transpose.map {|x| x.reduce(:+)}
+        break if board[location[0]][location[1]].color == color 
+        possibilities.push location
+        break if !board[location[0]][location[1]].color.nil? 
+        break if limit == 1
+      end
     end
-    possibilities.delete([y,x])
-    possibilities
-  end
-
-  def vertical_moves(y,x)
-    possibilities = []
-    counter = 0 
-    until counter >= 8
-      possibilities << [counter,x]
-      counter += 1
-    end
-    possibilities.delete([y,x])
     possibilities
   end
 end
