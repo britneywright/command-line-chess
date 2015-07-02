4540f810b27ec97fee5742774eeb5ffbff6b1d72
Britney Wright <blw06g@gmail.com>
2015-07-01 22:06:09 -0400

    create module for diagonal chessman moves

---
+++ lib/chess/diagonal_movable.rb
@@ -0,0 +1,45 @@
+module DiagonalMovable
+  def diagonal_moves(y,x)
+    right_down(y,x) + left_down(y,x) + left_up(y,x) + right_up(y,x)
+  end
+
+  def right_down(y,x)
+    possibilities = []
+    counter = 1 
+    until (y + counter >= 8 || x + counter >= 8)
+      possibilities << [y+counter,x+counter]
+      counter += 1
+    end
+    possibilities
+  end
+
+  def left_down(y,x)
+    possibilities = []
+    counter = 1 
+    until (y + counter >= 8 || x - counter < 0)
+      possibilities << [y+counter,x-counter]
+      counter += 1
+    end
+    possibilities
+  end
+
+  def right_up(y,x)
+    possibilities = []
+    counter = 1 
+    until (x + counter >= 8 || y - counter < 0)
+      possibilities << [y-counter,x+counter]
+      counter += 1
+    end
+    possibilities
+  end
+
+  def left_up(y,x)
+    possibilities = []
+    counter = 1 
+    until (y - counter < 0 || x - counter < 0)
+      possibilities << [y-counter,x-counter]
+      counter += 1
+    end
+    possibilities
+  end
+end
